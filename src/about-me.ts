45b7a85f2de9583c832cc57adcedffe878050773
Ilshat07 <98652504+Ilshat07@users.noreply.github.com>
2025-01-19 21:43:32 +0500

    Update about-me.ts

--- src/about-me.ts
+++ src/about-me.ts
@@ -1,6 +1,6 @@
 export const contactInfo = {
-    name: 'Имя Фамилия', //Укажите здесь имя
-    phoneNumber: '+77713011111', //Укажите здесь свой номер телефона
-    email: 'yourmail@gmail.com', //Укажите здесь свой email
-    bio: '' //Напишите сопроводительное письмо(от 50 символов!!!)
+    name: 'Ilshat Mansurov', //Укажите здесь имя
+    phoneNumber: '+77058214503', //Укажите здесь свой номер телефона
+    email: 'ilshatman2003@mail.ru', //Укажите здесь свой email
+    bio: 'I am a 4th year student at KBTU majoring in information systems. I am looking for a place where I can do my pre-graduation internship. I would like to do it with you.' //Напишите сопроводительное письмо(от 50 символов!!!)
 }  
